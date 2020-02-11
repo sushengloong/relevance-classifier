a901d3f56197ef8d54846c437b62f4e4a3969e94
Sheng-Loong Su <sushengloong@gmail.com>
2020-02-11 15:56:50 +0800

    initial commit - model training

---
+++ model.py
@@ -0,0 +1,32 @@
+import sqlite3
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+
+con = sqlite3.connect('db.sqlite')
+cur = con.cursor()
+cur.execute('select text, class_label from data')
+data = cur.fetchall()
+con.close()
+assert len(data) > 0
+
+X, y = (list(t) for t in zip(*data))
+assert len(data) == len(X) == len(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+model = Pipeline([
+    ('countvect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('sgd', SGDClassifier())
+])
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(f'Accuracy score: {accuracy_score(y_test, y_pred)}')
